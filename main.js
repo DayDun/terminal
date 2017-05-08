b5ec9b783625d8ab19f1c889f23c57123f81b1c6
Carl Sundvall <TheDayDun@gmail.com>
2017-05-08 21:42:56 +0200

    Idk what has changed
    lol

--- main.js
+++ main.js
@@ -1,46 +1,19 @@
-/*
-  Greethings, this is my cool text block. Feel free to touch it
-*/
-
-var charSet = [
-  "\u0000", "\u263a", "\u263b", "\u2665", "\u2666", "\u2663", "\u2660", "\u2022", "\u25d8", "\u25cb", "\u25d9", "\u2642", "\u2640", "\u266a", "\u266b", "\u263c",
-  "\u25ba", "\u25c4", "\u2195", "\u203c", "\u00b6", "\u00a7", "\u25ac", "\u21a8", "\u2191", "\u2193", "\u2192", "\u2190", "\u221f", "\u2194", "\u25b2", "\u25bc",
-  " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",  "-", ".", "/",
-  "0", "1", "2",  "3", "4", "5", "6", "7", "8", "9", ":", ";", "<",  "=", ">", "?",
-  "@", "A", "B",  "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",  "M", "N", "O",
-  "P", "Q", "R",  "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_",
-  "`", "a", "b",  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",  "m", "n", "o",
-  "p", "q", "r",  "s", "t", "u", "v", "w", "x", "y", "z", "{", "|",  "}", "~", "⌂",
-  "Ç", "ü", "é",  "â", "ä", "à", "å", "ç", "ê", "ë", "è", "ï", "î",  "ì", "Ä", "Å",
-  "É", "æ", "Æ",  "ô", "ö", "ò", "û", "ù", "ÿ", "Ö", "Ü", "¢", "£",  "¥", "₧", "ƒ",
-  "á", "í", "ó",  "ú", "ñ", "Ñ", "ª", "º", "¿", "⌐", "¬", "½", "¼",  "¡", "«", "»",
-  "░", "▒", "▓", "│", "┤", "╡", "╢", "╖", "╕", "╣", "║", "╗", "╝", "╜", "╛", "┐",
-  "└", "┴", "┬", "├", "─", "┼", "╞", "╟", "╚", "╔", "╩", "╦", "╠", "═", "╬", "╧",
-  "╨", "╤", "╥", "╙", "╘", "╒", "╓", "╫", "╪", "┘", "┌", "█", "▄", "▌", "▐", "▀",
-  "α", "ß", "Γ", "π", "Σ", "σ", "µ", "τ", "Φ", "Θ", "Ω", "δ", "∞", "φ", "ε", "∩",
-  "≡", "±", "≥", "≤", "⌠", "⌡", "÷", "≈", "°", "∙", "·", "√", "ⁿ", "²", "■", "\u00a0"
-];
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
 
 var api = {
-  setCursorPos: function(x, y) {
-    if (typeof x == "number" && x >= 0 && x < computers[0].width) {
-      computers[0].cursorPos[0] = x;
-    }
-    if (typeof y == "number" && y >= 0 && y < computers[0].height) {
-      computers[0].cursorPos[1] = y;
-    }
-  },
   print: function(text, x, y, bgColor, fgColor) {
     computers[0].print(text, x, y, bgColor, fgColor);
   },
-  println: function(text, x, y, bgColor, fgColor) {
-    computers[0].println(text, x, y, bgColor, fgColor);
-  },
-  debug: function() {
-    console.log.apply(null, arguments);
-  },
   clear: function(color) {
     computers[0].clear(color);
+  },
+  clearln: function(line, color) {
+    computers[0].clearln(line, color);
+  },
+  setSize: function(width, height) {
+    
   }
 };
 
@@ -49,13 +22,27 @@ var activeComputer = 0;
 
 function Computer() {
   this.id = computers.push(this) - 1;
-  this.width = 50;
-  this.height = 20;
+  this.width = 80;
+  this.height = 30;
   this.cursorPos = [0, 0];
   this.bgColor = 0x0;
   this.fgColor = 0xf;
   this.monitor = new Uint8Array(this.width * this.height * 3);
+  
   this.plugin = new jailed.Plugin("http://daydun.com/terminal/plugin.js", api);
+  this.plugin.whenConnected(function() {
+    window.addEventListener("keypress", function(event) {
+      if (event.which != 13) {
+        this.plugin.remote.event("char", {char: event.key});
+      }
+    }.bind(this));
+    window.addEventListener("keydown", function(event) {
+      this.plugin.remote.event("key", {key: event.which});
+      if (event.which == 8) {
+        event.preventDefault();
+      }
+    }.bind(this));
+  }.bind(this));
 }
 
 Computer.prototype.resize = function(width, height) {
@@ -65,69 +52,26 @@ Computer.prototype.resize = function(width, height) {
     // TODO: handle canvas resize
   }
 };
-
+Computer.prototype.runFile = function(path) {
+  var file = this.getFile(path);
+  if (file) {
+    this.plugin.remote.run(file);
+  }
+};
 Computer.prototype.clear = function(color) {
-  if (typeof color != "number" || color < 0 || color >= 256) {
-    color = this.bgColor;
-  }
-  for (var i=0; i<this.width * this.height; i++) {
-    switch(Math.floor(i / (this.width / this.height))) {
-      case 0:
-        this.monitor[i] = 0;
-        break;
-      case 1:
-        this.monitor[i] = color;
-        break;
-      case 2:
-        this.monitor[i] = this.fgColor;
-        break;
-    }
-  }
   renderer.clear(color);
 };
-
+Computer.prototype.clearln = function(line, color) {
+  renderer.clearln(line, color);
+};
 Computer.prototype.print = function(text, x, y, bgColor, fgColor) {
-  if (typeof x != "number" || x < 0 || x >= this.width) {
-    x = this.cursorPos[0];
-  }
-  if (typeof y != "number" || y < 0 || y >= this.height) {
-    y = this.cursorPos[1];
-  }
   var index = x + y * this.width;
-  if (typeof bgColor != "number" || bgColor < 0 || bgColor >= 256) {
-    bgColor = this.bgColor;
-  }
-  if (typeof fgColor != "number" || fgColor < 0 || fgColor >= 256) {
-    fgColor = this.fgColor;
-  }
-  if (text && typeof text == "number" && text >= 0 && text < 256) {
-    text = [text];
-  } else if (typeof text == "string" && text.length) {
-    var array = [];
-    for (var i=0; i<text.length; i++) {
-      array.push(charSet.indexOf(text.charAt(i)) || 0);
-    }
-    text = array;
-  } else {
-    text = [0];
-  }
   for (var j=0; j<text.length; j++) {
     this.monitor[(index + j) % (this.width * this.height)] = text[j];
     this.monitor[((index + j) % (this.width * this.height)) * (this.width * this.height)] = bgColor;
     this.monitor[((index + j) % (this.width * this.height)) * (this.width * this.height * 2)] = fgColor;
-    renderer.drawChar(text[j], (index + j) % this.width, Math.floor((index + j) / this.width) % this.height, bgColor, fgColor);
+    renderer.drawChar(text[j], mod((index + j), this.width), mod(Math.floor((index + j) / this.width), this.height), bgColor, fgColor);
   }
-  this.cursorPos[0] = (x + text.length) % this.width;
-  this.cursorPos[1] = Math.floor((index + text.length) / this.width) % this.height;
-}
-
-Computer.prototype.println = function(text, x, y, bgColor, fgColor) {
-  if (typeof x != "number" || x < 0 || x >= this.width) {
-    x = this.cursorPos[0];
-  }
-  this.print(text, x, y, bgColor, fgColor);
-  this.cursorPos[0] = x;
-  this.cursorPos[1] = (this.cursorPos[1] + 1) % this.height;
 }
 
 // FILESYSTEM
@@ -181,7 +125,7 @@ var fontSize = [8, 16];
 tintCanvas.width = fontSize[0];
 tintCanvas.height = fontSize[1];
 var tintCtx = tintCanvas.getContext("2d");
-/*function tintChar(char, color) {
+/ *function tintChar(char, color) {
   tintCtx.fillStyle = "#" + ("000000" + color.toString(16)).slice(-6);
   tintCtx.fillRect(0, 0, fontSize[0], fontSize[1]);
   tintCtx.globalCompositeOperation = "destination-atop";
@@ -213,32 +157,24 @@ window.addEventListener("load", function() {
 function Renderer(computer) {
   this.computer = computer;
   this.canvas = document.getElementById("canvas");
-  this.canvas.width = 50 * fontSize[0];
-  this.canvas.height = 20 * fontSize[1];
+  this.canvas.width = 80 * fontSize[0];
+  this.canvas.height = 30 * fontSize[1];
   this.ctx = canvas.getContext("2d");
   this.ctx.imageSmoothingEnabled = false;
   this.clear(0);
-  
-  this.random();
 }
-
-Renderer.prototype.random = function() {
-  for (var i=0; i<30; i++) {
-    for (var j=0; j<50; j++) {
-      this.drawChar(Math.floor(Math.random() * 256), j, i, 0x0, 0xf);
-    }
-  }
-};
 
 Renderer.prototype.getColor = function(index) {
   return "#" + ("000000" + palette[index].toString(16)).slice(-6);
 };
-
 Renderer.prototype.clear = function(color) {
   this.ctx.fillStyle = this.getColor(color);
   this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 };
-
+Renderer.prototype.clearln = function(line, color) {
+  this.ctx.fillStyle = this.getColor(color);
+  this.ctx.fillRect(0, line * fontSize[1], this.canvas.width, fontSize[1]);
+};
 Renderer.prototype.drawChar = function(char, x, y, bgColor, fgColor) {
   this.ctx.fillStyle = this.getColor(bgColor);
   this.ctx.fillRect(x * fontSize[0], y * fontSize[1], fontSize[0], fontSize[1]);
--- plugin.js
+++ plugin.js
@@ -1,10 +1,381 @@
-application.whenConnected(function() {
-  for (var i in application.remote) {
-    self[i] = application.remote[i];
-  }
-});
+Math.mod = function(n, m) {
+  return ((n % m) + m) % m;
+};
 
-application.setInterface({run: function(code) {
+
+/*
+  Greethings, this is my cool text block. Feel free to touch it
+*/
+
+var charSet = [
+  "\u0000", "\u263a", "\u263b", "\u2665", "\u2666", "\u2663", "\u2660", "\u2022", "\u25d8", "\u25cb", "\u25d9", "\u2642", "\u2640", "\u266a", "\u266b", "\u263c",
+  "\u25ba", "\u25c4", "\u2195", "\u203c", "\u00b6", "\u00a7", "\u25ac", "\u21a8", "\u2191", "\u2193", "\u2192", "\u2190", "\u221f", "\u2194", "\u25b2", "\u25bc",
+  " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",  "-", ".", "/",
+  "0", "1", "2",  "3", "4", "5", "6", "7", "8", "9", ":", ";", "<",  "=", ">", "?",
+  "@", "A", "B",  "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",  "M", "N", "O",
+  "P", "Q", "R",  "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_",
+  "`", "a", "b",  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",  "m", "n", "o",
+  "p", "q", "r",  "s", "t", "u", "v", "w", "x", "y", "z", "{", "|",  "}", "~", "⌂",
+  "Ç", "ü", "é",  "â", "ä", "à", "å", "ç", "ê", "ë", "è", "ï", "î",  "ì", "Ä", "Å",
+  "É", "æ", "Æ",  "ô", "ö", "ò", "û", "ù", "ÿ", "Ö", "Ü", "¢", "£",  "¥", "₧", "ƒ",
+  "á", "í", "ó",  "ú", "ñ", "Ñ", "ª", "º", "¿", "⌐", "¬", "½", "¼",  "¡", "«", "»",
+  "░", "▒", "▓", "│", "┤", "╡", "╢", "╖", "╕", "╣", "║", "╗", "╝", "╜", "╛", "┐",
+  "└", "┴", "┬", "├", "─", "┼", "╞", "╟", "╚", "╔", "╩", "╦", "╠", "═", "╬", "╧",
+  "╨", "╤", "╥", "╙", "╘", "╒", "╓", "╫", "╪", "┘", "┌", "█", "▄", "▌", "▐", "▀",
+  "α", "ß", "Γ", "π", "Σ", "σ", "µ", "τ", "Φ", "Θ", "Ω", "δ", "∞", "φ", "ε", "∩",
+  "≡", "±", "≥", "≤", "⌠", "⌡", "÷", "≈", "°", "∙", "·", "√", "ⁿ", "²", "■", "\u00a0"
+];
+
+
+var Keys = {
+  backspace: 8,
+  enter: 13,
+  end: 35,
+  home: 36,
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+  delete: 46
+};
+
+var eventListeners = {
+  "keyPress": [],
+  "keyDown": []
+};
+function addListener(event, action) {
+  if (event in eventListeners) {
+    eventListeners[event].push(action);
+  } else {
+    eventListeners[event] = [action];
+  }
+}
+function removeListener(event, action) {
+  if (event in eventListeners) {
+    eventListeners[event].splice(eventListeners[event].indexOf(action), 1);
+  }
+}
+function fireEvent(event, data) {
+  if (event in eventListeners) {
+    for (var i=0; i<eventListeners[event].length; i++) {
+      eventListeners[event][i](data);
+    }
+  }
+}
+
+
+var computer = {};
+computer.width = 80;
+computer.height = 30;
+computer.cursorPos = [0, 0];
+computer.bgColor = 0;
+computer.fgColor = 15;
+computer.monitor = new Uint8Array(computer.width * computer.height * 3);
+
+computer.files = [
+`
+// TODO: Support Insert text mode and text selection
+function read(history, callback) {
+  var output = "";
+  var pos = 0;
+  var caret = false;
+
+  var x = term.getCursorPos()[0];
+  var y = term.getCursorPos()[1];
+
+  function draw(index, drawCaret) {
+    if (drawCaret) {
+      term.print(output.charAt(index) || 0, x + index, y, 15, 0);
+    } else {
+      term.print(output.charAt(index) || 0, x + index, y);
+    }
+  }
+
+  function redraw() {
+    term.print(output, x, y);
+    if (caret) {
+      term.print(output.charAt(pos) || 0, x + pos, y, 15, 0);
+    }
+  }
+
+  var charListener = function(data) {
+    output = output.slice(0, pos) + data.char + output.slice(pos);
+    pos++;
+    enableCaret();
+    redraw();
+  };
+  var keyListener = function(data) {
+    switch(data.key) {
+      case Keys.enter:
+        removeListener("char", charListener);
+        removeListener("key", keyListener);
+        clearTimeout(caretTimeout);
+        draw(pos);
+        callback(output);
+        break;
+      case Keys.backspace:
+        if (pos > 0) {
+          output = output.slice(0, pos - 1) + output.slice(pos);
+          pos--;
+          draw(output.length);
+          draw(output.length + 1);
+          enableCaret();
+          redraw();
+        }
+        break;
+      case Keys.delete:
+        output = output.slice(0, pos) + output.slice(pos + 1);
+        enableCaret();
+        draw(output.length);
+        redraw();
+        break;
+      case Keys.left:
+        if (pos > 0) {
+          pos--;
+          enableCaret();
+          draw(pos + 1);
+        }
+        break;
+      case Keys.right:
+        if (pos < output.length) {
+          pos++;
+          enableCaret();
+          draw(pos - 1);
+        }
+        break;
+      case Keys.home:
+        draw(pos);
+        pos = 0;
+        enableCaret();
+        break;
+      case Keys.end:
+        draw(pos);
+        pos = output.length;
+        enableCaret();
+        break;
+    }
+  };
+  addListener("char", charListener);
+  addListener("key", keyListener);
+  var toggleCaret = function() {
+    caret = !caret;
+    draw(pos, caret);
+    caretTimeout = setTimeout(toggleCaret, 500);
+  };
+  var caretTimeout = setTimeout(toggleCaret, 500);
+  function enableCaret() {
+    clearTimeout(caretTimeout);
+    caret = true;
+    draw(pos, caret);
+    caretTimeout = setTimeout(toggleCaret, 500);
+  }
+}
+
+var path = "/";
+
+term.println("DayDun Terminal 0.1");
+term.print(path + ">");
+
+var caretStart = term.getCursorPos();
+
+function parseInput(output) {
+  term.setCursorPos(0, caretStart[1] + Math.floor((caretStart[0] + output.length) / term.getSize()[0]) + 1);
+  /*output = output.split(" ");
+  if (output[0] == "hello") {
+    term.println("hello world!");
+  } else if (output[0] == "help") {
+    term.println("hello");
+    term.println("help");
+    term.println("ls");
+  } else if (output[0] == "ls") {
+    term.println("init shell");
+  }*/
+  try {
+    eval(output);
+  } catch(e) {
+    term.println("error", null, null, null, 0xc);
+  }
+  term.print(path + ">");
+  caretStart = term.getCursorPos();
+  read(48, parseInput);
+}
+
+read(48, parseInput);
+`
+];
+computer.fileSystem = {
+  init: {id: 0},
+  shell: {children: {
+    main: {id: 1},
+    programs: {children: {
+      cd: {id: 2},
+      clear: {id: 3},
+      copy: {id: 4},
+      delete: {id: 5},
+      edit: {id: 6},
+      help: {id: 7},
+      javascript: {id: 8},
+      list: {id: 8},
+      mkdir: {id: 9},
+      move: {id: 10},
+      rename: {id: 11}
+    }}
+  }}
+};
+computer.getPath = function(path) {
+  var nodes = path.split("/").filter(String);
+  var node = {children: this.fileSystem};
+  for (var i=0; i<nodes.length; i++) {
+    if (nodes[i] in node.children) {
+      node = node.children[nodes[i]];
+      if (!("children" in node)) {
+        if (i == nodes.length - 1) {
+          break;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+  return node;
+};
+
+var os = {};
+os.getFile = function(path) {
+  var file = computer.getPath(path);
+  if (file && "id" in file) {
+    return computer.files[file.id];
+  } else {
+    return false;
+  }
+};
+os.runFile = function(path) {
+  var file = os.getFile(path);
+  if (file) {
+    run(file);
+  }
+};
+
+var term = {};
+term.clear = function(color) {
+  if (typeof color != "number" || color < 0 || color >= 256) {
+    color = computer.bgColor;
+  }
+  application.remote.clear(color);
+  
+  for (var i=0; i<computer.width * computer.height; i++) {
+    computer.monitor[i] = 0;
+    computer.monitor[i + (computer.width * computer.height)] = color;
+    computer.monitor[i + (computer.width * computer.height * 1)] = computer.fgColor;
+  }
+};
+term.clearln = function(line, color) {
+  if (typeof line != "number") {
+    line = computer.cursorPos[1];
+  }
+  if (typeof color != "number" || color < 0 || color >= 256) {
+    color = computer.bgColor;
+  }
+  application.remote.clearln(Math.mod(line, computer.height), color);
+  
+  for (var i=Math.mod(line, computer.height) * computer.width; i<Math.mod(line + 1, computer.height) * computer.width; i++) {
+    computer.monitor[i] = 0;
+    computer.monitor[i + (computer.width * computer.height)] = color;
+    computer.monitor[i + (computer.width * computer.height * 2)] = computer.fgColor;
+  }
+};
+term.getCursorPos = function() {
+  return computer.cursorPos;
+};
+term.setCursorPos = function(x, y) {
+  if (typeof x == "number") {
+    computer.cursorPos[0] = Math.mod(x, computer.width);
+  }
+  if (typeof y == "number") {
+    computer.cursorPos[1] = Math.mod(y, computer.height);
+  }
+  return computer.cursorPos;
+};
+term.getSize = function() {
+  return [computer.width, computer.height];
+};
+term.setSize = function(width, height) {
+  if (typeof width == "number" && width >= 1) {
+    computer.width = width;
+  }
+  if (typeof height == "number" && height >= 1) {
+    computer.height = height;
+  }
+  application.remote.setSize(computer.width, computer.height);
+  return [computer.width, computer.height];
+};
+term.print = function(text, x, y, bgColor, fgColor) {
+  if (typeof x != "number") {
+    x = computer.cursorPos[0];
+  }
+  if (typeof y != "number") {
+    y = computer.cursorPos[1];
+  }
+  
+  if (typeof bgColor != "number" || bgColor < 0 || bgColor >= 256) {
+    bgColor = computer.bgColor;
+  }
+  if (typeof fgColor != "number" || fgColor < 0 || fgColor >= 256) {
+    fgColor = computer.fgColor;
+  }
+  
+  if (typeof text == "number") {
+    text = [[text]];
+  } else if (typeof text == "string" && text.length) {
+    var array = [];
+    var textArray = [];
+    for (var i=0; i<text.length; i++) {
+      if (text.charAt(i) == "\n") {
+        array.push(textArray);
+        textArray = [];
+      } else {
+        textArray.push(charSet.indexOf(text.charAt(i)) || 0);
+      }
+    }
+    array.push(textArray);
+    text = array;
+  } else {
+    text = [[0]];
+  }
+  
+  computer.cursorPos = [x, y];
+  for (var j=0; j<text.length; j++) {
+    application.remote.print(text[j], computer.cursorPos[0], computer.cursorPos[1], bgColor, fgColor);
+    
+    computer.cursorPos[1] += Math.floor((x + text[j].length) / computer.width);
+    if (j == text.length - 1) {
+      computer.cursorPos[0] = Math.mod(x + text[j].length, computer.width);
+    } else {
+      computer.cursorPos[0] = x;
+      computer.cursorPos[1]++;
+    }
+  }
+  
+};
+term.println = function(text, x, y, bgColor, fgColor) {
+  term.print(text + "\n", x, y, bgColor, fgColor);
+};
+term.scroll = function(amount, wrap) {
+  for (var i=0; i<computer.height; i++) {
+    for (var j=0; j<computer.width; j++) {
+      if (wrap) {
+        computer.monitor[Math.mod(i - amount, computer.height) * computer.width + j] = computer.monitor[i * computer.width + j];
+      } else {
+        computer.monitor[(i - amount) * computer.width + j] = computer.monitor[i * computer.width + j];
+      }
+    }
+  }
+};
+
+
+var run = function(code) {
   var indexedDB = void 0;
   var location = void 0;
   var navigator = void 0;
@@ -28,18 +399,38 @@ application.setInterface({run: function(code) {
   var ononline = void 0;
   var importScripts = void 0;
   var console = void 0;
+  var isSecureContext = void 0;
+  var onrejectionhandled = void 0;
+  var onunhandledrejection = void 0;
   var self = void 0;
-  
+
+  var eventListeners = void 0;
+  var computer = void 0;
+  var charSet = void 0;
+  var run = void 0;
+
+  // crypto
+
   var application = void 0;
-  
+
   try {
     eval(code);
   } catch(e) {
-    log(e);
-    print(e.message, e.stack);
+    debug(e);
+    term.println(e.message, null, null, 0, 0xC);
   }
-}.bind({})});
+}.bind({});
 
-function log() {
-  self.console.log(arguments);
+application.setInterface({
+  run: run,
+  event: function(event, data) {
+    fireEvent(event, data);
+  }
+});
+
+function debug() {
+  self.console.log.apply(null, arguments);
 }
+
+
+os.runFile("/init");
